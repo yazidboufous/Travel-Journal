887d0d8ee264ab516e5840b5384422ba4e24fcdc
Yazid Boufous <96538633+yazidboufous@users.noreply.github.com>
2022-09-02 20:39:39 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './components/Navbar.js'
+import Card from './components/Card.js'
+import data from './data.js'
+
+
+export default function App(){
+    
+    const cards= data.map(item=>{
+        return <Card
+            id={item.id}
+            item={item}
+        />
+    }
+    )
+    
+    return(
+        <div>
+            <Navbar/>
+                <div className="card-list">
+                   {cards}
+                </div>
+        </div>
+    )
+}
